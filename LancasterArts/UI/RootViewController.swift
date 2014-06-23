fefa04969a804db36fa2e148312aecd0f4461a85
Sean Overby <Sean.Overby@capitalone.com>
2014-06-23 10:52:46 -0400

    Initial Commit

---
+++ LancasterArts/UI/RootViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RootViewController.swift
+//  LancasterArts
+//
+//  Created by Overby, Sean on 6/12/14.
+//  Copyright (c) 2014 Sean Overby. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+                            
+    @IBOutlet var rootScrollView : UIScrollView
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
